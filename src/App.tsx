019d554fe58408a7d7e8d33cb9a5665c090c93d0
amiroshnichenko <amiroshnichenko@sfxdx.com>
2023-02-02 09:46:36 +0700

    Add ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Suspense } from 'react';
 import { Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
-import { Counter } from './components/Counter/Counter';
-import './index.scss';
+import './styles/index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { useTheme } from './theme/useTheme';
+
 
 export default function App() {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
       <Link to='/'>Main</Link>
       <Link to='/about'>About</Link>
+      <button onClick={toggleTheme}>Toggle</button>
       <Suspense fallback={<div>Загрузка...</div>}>
       <Routes>
         <Route path={'/'} element={<MainPageAsync />} />
@@ -18,8 +21,6 @@ export default function App() {
 
         </Routes>
       </Suspense>
-      dwdwdwd
-      <Counter />
     </div>
   )
 }
