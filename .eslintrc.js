cfa4bddfadf047355bf58a8759ed527d2e9e90e9
amiroshnichenko <amiroshnichenko@sfxdx.com>
2023-05-12 21:51:20 +0700

    Fix rule in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -118,7 +118,7 @@ module.exports = {
                 },
             },
         ],
-        'react/jsx-max-props-per-line': ['error', { maximum: 3 }],
+        'react/jsx-max-props-per-line': ['error', { maximum: 4 }],
         'react/no-unstable-nested-components': 'warn',
     },
     globals: {
