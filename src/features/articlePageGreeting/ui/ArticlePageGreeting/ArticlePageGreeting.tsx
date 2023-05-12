f9e85e7f8d6542c126211ff0dc2e906d323f71ef
amiroshnichenko <amiroshnichenko@sfxdx.com>
2023-05-12 14:37:20 +0700

    Add ArticlePageGreeting

---
+++ src/features/articlePageGreeting/ui/ArticlePageGreeting/ArticlePageGreeting.tsx
@@ -0,0 +1,55 @@
+import { memo, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { saveJsonSettings, useJsonSettings } from '@/entities/User';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { detectDevice } from '@/shared/lib/detectDevice/detectDevice';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { Drawer } from '@/shared/ui/Drawer';
+import { Modal } from '@/shared/ui/Modal';
+import { Text } from '@/shared/ui/Text';
+
+export const ArticlePageGreeting = memo(() => {
+    const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+    const [isOpen, setIsOpen] = useState(false);
+    const { isArticlesPageWasOpened } = useJsonSettings();
+    const isMobile = detectDevice();
+
+    useEffect(() => {
+        if (!isArticlesPageWasOpened) {
+            setIsOpen(true);
+            dispatch(saveJsonSettings({ isArticlesPageWasOpened: true }));
+        }
+    }, [dispatch, isArticlesPageWasOpened]);
+
+    const onClose = () => setIsOpen(false);
+
+    const text = (
+        <Text
+            title={t('Добро пожаловать на страницу статей')}
+            text={t(
+                'Здесь вы можете искать и просматривать статьи на различные темы',
+            )}
+        />
+    );
+
+    if (isMobile) {
+        return (
+            <Drawer isOpen={isOpen} lazy onClose={onClose}>
+                {text}
+            </Drawer>
+        );
+    }
+
+    return (
+        <Modal
+            lazy
+            isOpen={isOpen}
+            onClose={onClose}
+            className={classNames('', {}, [])}
+        >
+            {text}
+        </Modal>
+    );
+});
